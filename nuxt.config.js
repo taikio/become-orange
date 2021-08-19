3c46e6be2f4721ff8a7420299d4d8c9c82f7b05e
welker.ferreira <welker.ferreira@hubconexa.com>
2021-08-19 08:38:23 -0300

    ajustando configuração de build

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  router: { base: '/become-orange-site/'},
+  router: { base: '/become-orange/'},
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
