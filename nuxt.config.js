ea66edd881478476c1413daa0e424ce7fbff508a
welker.ferreira <welker.ferreira@hubconexa.com>
2021-08-19 08:15:13 -0300

    feat: configurando deploy com github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
+  router: { base: '/become-orange-site/'},
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
